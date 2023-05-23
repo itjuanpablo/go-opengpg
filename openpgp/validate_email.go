875528a97340188ecf815a50248b52e933669c91
Juan Pablo Ladeira <juan.ladeira@riversoft.com.br>
2023-05-23 17:59:42 -0300

    MOD: Interação no terminal e validação de email

---
+++ openpgp/validate_email.go
@@ -0,0 +1,11 @@
+package openpgp
+
+import "regexp"
+
+// ValidateEmail faz a validação do formato do e-mail digitado
+func ValidateEmail(email string) bool {
+	// Expressão regular para validar o formato do e-mail
+	emailRegex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	match, _ := regexp.MatchString(emailRegex, email)
+	return match
+}
