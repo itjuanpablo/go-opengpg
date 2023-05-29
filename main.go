70ac77666a3e5e6d5ddacf3600282df5879d8f03
Juan Pablo Ladeira <juan.ladeira@riversoft.com.br>
2023-05-29 16:02:44 -0300

    MOD: Refatoração das flags e lógicas.

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 	"time"
 
 	"openpgp/utils"
@@ -14,7 +17,7 @@ var (
 	name            string
 	comment         string
 	email           string
-	bits            string
+	bitsKey         int
 	passphrase      string
 	expirationInput string
 	expirationTime  time.Time
@@ -43,37 +46,166 @@ func main() {
 			usage()
 			return
 		}
-	}
 
-	fs := flag.NewFlagSet("encrypt [flags]", flag.ExitOnError)
+		fs := flag.NewFlagSet("encrypt [flags]", flag.ExitOnError)
 
-	fs.StringVar(&fileToEncrypt, "file", "", "[File to encrypt]")
-	sygn := fs.Bool("sygn", false, "[Enter password to sign encrypted file]")
+		fs.StringVar(&fileToEncrypt, "file", "", "[File to encrypt]")
+		sygn := fs.Bool("sygn", false, "[Enter password to sign encrypted file]")
 
-	fs.Parse(flag.Args()[1:])
+		fs.Parse(flag.Args()[1:])
 
-	if *sygn {
+		if *sygn {
+			if privKey == "" {
+				fmt.Println("Error: -privKey is required")
+				usage()
+				return
+			}
+
+			for passphrase == "" {
+				fmt.Print("Passphrase for sygn: ")
+				fmt.Scanln(&passphrase)
+			}
+
+			// Encriptar arquivo
+			err := utils.EncryptSygnMessageArmored(pubKey, privKey, passphrase, fileToEncrypt)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		} else {
+			err := utils.EncryptFileArmored(pubKey, fileToEncrypt)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		}
+
+	case flag.Arg(0) == "decrypt":
+
 		if privKey == "" {
 			fmt.Println("Error: -privKey is required")
 			usage()
 			return
 		}
 
-		for passphrase == "" {
-			fmt.Print("Passphrase for sygn: ")
+		fs := flag.NewFlagSet("decrypt [flags]", flag.ExitOnError)
+
+		fs.StringVar(&fileToDecrypt, "file", "", "[File to decrypt]")
+		verify := fs.Bool("verify", false, "[Enter password to sign decrypted file]")
+
+		fs.Parse(flag.Args()[1:])
+
+		if *verify {
+			if pubKey == "" {
+				fmt.Println("Error: -pubKey is required")
+				usage()
+				return
+			}
+
+			for passphrase == "" {
+				fmt.Print("Passphrase: ")
+				fmt.Scanln(&passphrase)
+			}
+			err := utils.DecrytptVerifyMessageArmored(pubKey, privKey, passphrase, fileToDecrypt)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		} else {
+			fmt.Print("Passphrase (''): ")
+			fmt.Scanln(&passphrase)
+			err := utils.DecryptMessageArmored(privKey, fileToDecrypt, passphrase)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		}
+
+	case flag.Arg(0) == "keygen":
+
+		fs := flag.NewFlagSet("keygen [Generates a new pub/priv key pair]", flag.ExitOnError)
+
+		fs.StringVar(&keyOutputDir, "d", ".keys/", "Directory of keys files")
+		pass := fs.Bool("passphrase", false, "[Define passphrase is on]")
+		expTime := fs.Bool("expiration", false, "[Define expiration time of the key is on]")
+
+		fs.Parse(flag.Args()[1:])
+
+		// Caminho do arquivo de salvamento das chaves
+		fmt.Printf("Directory and prefix keys ('./key-defaultname') ")
+		fmt.Scanln(&directory)
+		if directory == "" {
+			directory = ".key-defaultname"
+		}
+
+		reader := bufio.NewReader(os.Stdin)
+
+		fmt.Print("Name: ")
+		name, _ = reader.ReadString('\n')
+
+		fmt.Print("Comment: ")
+		comment, _ = reader.ReadString('\n')
+
+		for {
+			fmt.Printf("Email adress: ")
+			fmt.Scanln(&email)
+			if ok := utils.ValidateEmail(email); !ok {
+				fmt.Println("Valid email, type again: ")
+				continue
+			} else {
+				break
+			}
+		}
+
+		fmt.Printf("Your User ID is: \n \n %s (%s) <%s>\"\n", strings.TrimSpace(name), strings.TrimSpace(comment), strings.TrimSpace(email))
+
+		for bitsKey != 1024 && bitsKey != 2048 && bitsKey != 4096 {
+			fmt.Println("Number bites of the keys: \n[1024 bits] [2048 bits] [4096 bits]")
+			fmt.Printf("Type the size (2048): ")
+			fmt.Scanln(&bitsKey)
+			if bitsKey == 0 {
+				bitsKey = 2048
+			}
+		}
+
+		if *pass {
+			fmt.Printf("Passphrase(''): ")
 			fmt.Scanln(&passphrase)
 		}
 
-		// Encriptar chave
-		err := utils.EncryptSygnMessageArmored(pubKey, privKey, passphrase, fileToEncrypt)
-		if err != nil {
-			log.Fatal(err.Error())
+		for {
+			if *expTime {
+				fmt.Println(`
+				Specify how long the key should be valid
+				0 = key not expires
+				d = key expires n days
+				w = key expires n weeks
+				m = key expires n months
+				y = key expires n years
+				`)
+				fmt.Printf("Is the key valid for? (0)")
+				fmt.Scanln(&expirationInput)
+				if expirationInput != "" {
+					dataExp, err := utils.ParseKeyDuration(expirationInput)
+					if err != nil {
+						fmt.Println("Invalid input: ", err)
+						continue
+					}
+					fmt.Printf("Key expires: %s", dataExp.Format("02/02/2006 15:04:05"))
+					expirationTime = dataExp
+					break
+				} else {
+					expirationInput = "0"
+					fmt.Println("Key not expires.")
+					break
+				}
+			}
 		}
-	} else {
-		err := utils.EncryptFileArmored(pubKey, fileToEncrypt)
+
+		err := utils.GenerateKeys(directory, name, comment, email, passphrase, &expirationTime, bitsKey)
 		if err != nil {
-			log.Fatal(err.Error())
+			log.Fatal(err)
 		}
+
+	default:
+		usage()
+		return
 	}
 }
 
