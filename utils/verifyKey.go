3620c308cb5de604634613fc256f4f6236e672d8
Juan Pablo Ladeira <juan.ladeira@riversoft.com.br>
2023-06-07 17:47:48 -0300

    ADD: Criação da lógica para verificação de chaves

--- utils/verifyKey.go
+++ utils/verifyKey.go
@@ -3,46 +3,53 @@ package utils
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/ProtonMail/go-crypto/openpgp"
 )
 
-func verifyOpenPGPKey(keyringFile string, keyID string) error {
-	// Abra o arquivo de keyring (anéis de chave) OpenPGP
-	file, err := os.Open(keyringFile)
+// Acessar local do arquivo onde o par de chaves
+// Ler conteúdo das chaves
+// Verificar se é uma chave OpenPGP
+// Verificar expiração da chave
+
+func CheckKeys(keyFile string) error {
+	// Abrir arquivo
+	file, err := os.Open(keyFile)
 	if err != nil {
-		return fmt.Errorf("falha ao abrir o arquivo de keyring: %s", err)
+		return fmt.Errorf("Falha ao abrir arquivo de chave", err)
 	}
 	defer file.Close()
 
-	// Leia o keyring OpenPGP
-	entityList, err := openpgp.ReadKeyRing(file)
+	// Ler arquivo de chave
+	entityList, err := openpgp.ReadArmoredKeyRing(file)
 	if err != nil {
-		return fmt.Errorf("falha ao ler o keyring: %s", err)
+		return fmt.Errorf("Falha ao ler arquivo de chave", err)
 	}
 
-	// Encontre a entidade com base no ID da chave
-	entity, ok := entityList.KeysById(keyID)
-	if !ok {
-		return fmt.Errorf("chave não encontrada no keyring")
+	// Verificar se é uma chave válida
+	entity := entityList[0]
+	privKey := entity.PrivateKey
+	if privKey == nil {
+		return fmt.Errorf("Não foi fornecida uma chave privada")
 	}
 
-	// Verifique a validade da chave
-	err = entity[0].Entity.PrimaryKey.VerifyUserIdSignature(entity[0].Entity.Identity.Name, entity[0].Entity.Identity.UserId.Id, nil)
-	if err != nil {
-		return fmt.Errorf("falha na verificação da chave: %s", err)
+	var expires time.Time
+	// for _, ident := range entity.Identities {
+	// 	if ident.SelfSignature != nil && ident.SelfSignature.KeyLifetimeSecs > 0 {
+	// 		creationTime := ident.SelfSignature.CreationTime
+	// 		lifetimeSecs := time.Duration(ident.SelfSignature.KeyLifetimeSecs) * time.Second
+	// 		expires = creationTime.Add(lifetimeSecs)
+	// 		break
+	// 	}
+	// }
+
+	// Verifica se a chave não tem data de expiração
+	if expires.IsZero() {
+		fmt.Println("A chave não possui data de expiração")
+	} else {
+		fmt.Printf("A chave expira em: %s\n", expires.Format(time.RFC3339))
 	}
 
-	fmt.Println("Chave válida e verificada com sucesso!")
 	return nil
 }
-
-// func main() {
-// 	keyringFile := "caminho/para/o/keyring.gpg"
-// 	keyID := "ID_DA_CHAVE"
-
-// 	err := verifyOpenPGPKey(keyringFile, keyID)
-// 	if err != nil {
-// 		fmt.Println("Erro:", err)
-// 		return
-// 	}
